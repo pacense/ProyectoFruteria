d9ae9f29b670b001145e77701a67e4af578a67f9
jesus <jesus@jesus>
2015-05-18 18:52:47 +0200

    bucador cliente

--- ProyectoFruteria/src/Vista/BuscarCliente.java
+++ ProyectoFruteria/src/Vista/BuscarCliente.java
@@ -5,65 +5,166 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Dimension;
 import java.awt.Component;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BuscarCliente extends JPanel {
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_2;
+	private JTextField textNombrecliente;
+	private JTextField textApellidocliente;
+	private JTextField textDNIcliente;
+	private JTextField textColorpelocliente;
+	private JButton btnNewButton;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_1;
 	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JLabel lblNewLabel_6;
 
 	/**
 	 * Create the panel.
 	 */
 	public BuscarCliente() {
-		setLayout(null);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 470, 0 };
+		gridBagLayout.rowHeights = new int[] { 372, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setFont(new Font("MV Boli", Font.PLAIN, 21));
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 0;
+		add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[] { 113, 120, 0, 72 };
+		gbl_panel_1.rowHeights = new int[] { 46, 36, 33, 34, 0, 69, 0, 0 };
+		gbl_panel_1.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0 };
+		gbl_panel_1.rowWeights = new double[] { 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0 };
+		panel_1.setLayout(gbl_panel_1);
+
+		lblNewLabel = new JLabel("BUSCADOR DE CLIENTES");
+		lblNewLabel.setFont(new Font("MV Boli", Font.PLAIN, 21));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel.gridwidth = 3;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		panel_1.add(lblNewLabel, gbc_lblNewLabel);
+
+		lblNewLabel_2 = new JLabel("Nombre cliente");
+		lblNewLabel_2.setFont(new Font("MV Boli", Font.PLAIN, 12));
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.NORTH;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 1;
+		panel_1.add(lblNewLabel_2, gbc_lblNewLabel_2);
+
+		textNombrecliente = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.anchor = GridBagConstraints.NORTH;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 1;
+		panel_1.add(textNombrecliente, gbc_textField);
+		textNombrecliente.setColumns(10);
+
+		lblNewLabel_3 = new JLabel("Apellido cliente");
+		lblNewLabel_3.setFont(new Font("MV Boli", Font.PLAIN, 12));
+		GridBagConstraints gbc_lblPrecioDelProducto = new GridBagConstraints();
+		gbc_lblPrecioDelProducto.anchor = GridBagConstraints.NORTH;
+		gbc_lblPrecioDelProducto.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPrecioDelProducto.gridx = 1;
+		gbc_lblPrecioDelProducto.gridy = 2;
+		panel_1.add(lblNewLabel_3, gbc_lblPrecioDelProducto);
+
+		textApellidocliente = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.anchor = GridBagConstraints.NORTH;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 2;
+		panel_1.add(textApellidocliente, gbc_textField_1);
+		textApellidocliente.setColumns(10);
+
+		lblNewLabel_4 = new JLabel("DNI cliente");
+		lblNewLabel_4.setFont(new Font("MV Boli", Font.PLAIN, 12));
+		GridBagConstraints gbc_lblDescripcin = new GridBagConstraints();
+		gbc_lblDescripcin.anchor = GridBagConstraints.NORTH;
+		gbc_lblDescripcin.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDescripcin.gridx = 1;
+		gbc_lblDescripcin.gridy = 3;
+		panel_1.add(lblNewLabel_4, gbc_lblDescripcin);
+
+		textDNIcliente = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.anchor = GridBagConstraints.NORTH;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 2;
+		gbc_textField_2.gridy = 3;
+		panel_1.add(textDNIcliente, gbc_textField_2);
+		textDNIcliente.setColumns(10);
+
+		btnNewButton = new JButton("Buscar cliente");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(1, 0, 450, 300);
-		add(panel);
-		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JLabel lblBuscadorDeClientes = new JLabel("BUSCADOR DE CLIENTES");
-		lblBuscadorDeClientes.setHorizontalAlignment(SwingConstants.CENTER);
-		lblBuscadorDeClientes.setPreferredSize(new Dimension(500, 500));
-		panel.add(lblBuscadorDeClientes);
-		
-		JLabel lblNewLabel = new JLabel("Nombre");
-		panel.add(lblNewLabel);
+		lblNewLabel_1 = new JLabel("pelo cliente");
+		lblNewLabel_1.setFont(new Font("MV Boli", Font.PLAIN, 12));
+		lblNewLabel_1.setPreferredSize(new Dimension(52, 14));
+		lblNewLabel_1.setMinimumSize(new Dimension(52, 14));
+		lblNewLabel_1.setMaximumSize(new Dimension(52, 14));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTH;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 4;
+		panel_1.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		textField = new JTextField();
-		panel.add(textField);
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 4;
+		panel_1.add(textField, gbc_textField);
 		textField.setColumns(10);
+		btnNewButton.setFont(new Font("MV Boli", Font.PLAIN, 12));
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 5;
+		panel_1.add(btnNewButton, gbc_btnNewButton);
 		
-		JLabel lblNewLabel_1 = new JLabel("Apellidos\r\n");
-		panel.add(lblNewLabel_1);
-		
-		textField_1 = new JTextField();
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblDni = new JLabel("DNI");
-		panel.add(lblDni);
-		
-		textField_2 = new JTextField();
-		panel.add(textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblColorPelo = new JLabel("Color pelo");
-		panel.add(lblColorPelo);
-		
-		textField_3 = new JTextField();
-		panel.add(textField_3);
-		textField_3.setColumns(10);
-		
-		JButton btnBuscar = new JButton("BUSCAR");
-		panel.add(btnBuscar);
-		
-		JLabel label_3 = new JLabel("");
-		panel.add(label_3);
+		lblNewLabel_6 = new JLabel("RESULTADO");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.anchor = GridBagConstraints.NORTH;
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_6.gridx = 2;
+		gbc_lblNewLabel_6.gridy = 6;
+		panel_1.add(lblNewLabel_6, gbc_lblNewLabel_6);
+
 	}
 }
